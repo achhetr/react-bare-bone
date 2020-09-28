b1d096cfdfa5de8b18f19a72dbb583e7a6330ecd
Akash Chhetri <akash.chhetri1@gmail.com>
2020-09-29 08:48:05 +1000

    react router added

--- src/components/MainApp.js
+++ src/components/MainApp.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import { APP_NAME } from '../personalise/config';
 
 const MainApp = () => {
+	const Header = () => <h1>{APP_NAME}</h1>;
+	const HomePage = () => <h2>This is a React Bare Bone App</h2>;
 	return (
-		<div>
-			<h1>Today is a lucky day</h1>
-		</div>
+		<BrowserRouter>
+			<div>
+				<Header />
+				<Switch>
+					<Route to="/" component={HomePage} />
+				</Switch>
+			</div>
+		</BrowserRouter>
 	);
 };
 
